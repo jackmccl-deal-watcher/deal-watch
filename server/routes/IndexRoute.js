5eef2654a759b1bb3f0302798d1e4856825220a6
Jack McClure <jackmccl@meta.com>
2025-06-24 12:37:19 -0700

    Installed express and nodemon, outlined server route file structure

---
+++ server/routes/IndexRoute.js
@@ -0,0 +1,34 @@
+// Start imports and setup
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const cors = require('cors')
+// Enable CORS for all routes
+app.use(cors())
+
+// End imports and setup
+
+// Start routes
+
+const UserRoutes = require('./UserRoutes.js')
+app.use('/api/UserRoutes', UserRoutes)
+
+// End routers
+
+// Start index functions
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    try {
+        res.send('Welcome to deal watch!')
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(error)
+    }
+})
+
+// End index functions
