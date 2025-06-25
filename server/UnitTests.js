77b9e3ea2b7ec9c57ca0b1c21e7335678c5c3088
Jack McClure <jackmccl@meta.com>
2025-06-24 18:38:31 -0700

    Added UserModel, getUser, and createUser

--- server/UnitTests.js
+++ server/UnitTests.js
@@ -1,23 +1,77 @@
-const mongoose_test = () => {
-    const mongoose = require('./Mongoose.js')
-
-    const UserSchema = new mongoose.Schema({
-        username: String,
-        password: String,
-    })
-
-    const UserModel = mongoose.model('User', UserSchema)
-
-    const newUser = new UserModel()
-
-    newUser.username = "first user!"
-    newUser.password = "test password"
-
-    newUser.save()
+const mongoose_connection_test = async () => {
+    try {
+        const mongoose = require('./Mongoose.js')
+        const UserModel = require('./models/UserModel.js')
+    
+        const newUser = new UserModel()
+    
+        newUser.username = "first user!"
+        newUser.password = "test password"
+    
+        await newUser.save()
+        
+        console.log("mongoose_connection_test: Passed")
+        return true
+    } catch (error) {
+        console.error("mongoose_connection_test: Failed")
+        return false
+    }
 }
 
-const running_tests = () => {
-    mongoose_test()
+const mongoose_getUser_test = async () => {
+    const { createUser, getUser } = require('./utils/UserUtils.js')
+    const UserModel = require('./models/UserModel.js')
+
+    const test_username = "test_user1"
+    const test_password = "test_password1"
+
+    const newUser = new UserModel()
+    
+    newUser.username = test_username
+    newUser.password = test_password
+
+    await newUser.save()
+
+    const user = await getUser(test_username)
+
+    console.log(user)
+
+    if (user.username && user.username === test_username 
+        && user.password && user.password == test_password) {
+        console.log("mongoose_getUser_test: Passed")
+        return true
+    } else {
+        console.error("mongoose_getUser_test: Failed")
+        return false
+    }
+}
+
+const mongoose_createUser_test = async () => {
+    const { createUser, getUser } = require('./utils/UserUtils.js')
+    const UserModel = require('./models/UserModel.js')
+
+    const test_username = "test_user1"
+    const test_password = "test_password1"
+
+    await UserModel.findOneAndDelete({ 'username': test_username })
+
+    await createUser(test_username, test_password)
+
+    const user = await UserModel.findOne({ 'username': test_username }, 'username password')
+
+    if (user.username && user.username === test_username 
+        && user.password && user.password == test_password) {
+        console.log("mongoose_createUser_test: Passed")
+        return true
+    } else {
+        console.error("mongoose_createUser_test: Failed")
+        return false
+    }
+}
+
+const running_tests = async () => {
+    await mongoose_getUser_test()
+    process.exit(0)
 }
 
 running_tests()
