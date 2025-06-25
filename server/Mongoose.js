5d94f16e382bac35cdefa1aba6437fc266304369
amotor95 <59101078+amotor95@users.noreply.github.com>
2025-06-24 18:59:59 -0700

    Revert "Jackmccl/login UserModel, getUser, createUser"

--- server/Mongoose.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DB_CONNECTION)
-
-module.exports = mongoose
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,4 +1,3 @@
-// Start imports and setup
 const express = require('express')
 
 const router = express.Router()
@@ -6,31 +5,6 @@ const router = express.Router()
 const cors = require('cors')
 // Enable CORS for all routes
 router.use(cors())
-
-const mongoose = require('../Mongoose.js')
-
-// End imports and setup
-
-// Start user routes
-
-// User login
-router.get('/login', async (req, res) => {
-    try {
-        const { username, password } = req.body
-        
-        if (board) {
-            res.status(200).json(board)
-        } else {
-            console.error(`BoardID: ${boardID} not found`)
-            res.status(404).json(`BoardID: ${boardID} not found`)
-        }
-    } catch (error) {
-        console.error(error)
-        res.status(500).json(error)
-    }
-})
-
-// End user routes
 
 
 module.exports = router
