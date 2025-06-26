c2003ca14267c9e871d9e5897f48da0b95857736
Jack McClure <jackmccl@meta.com>
2025-06-26 10:16:17 -0700

    Created UserProvider context to provide user between pages

--- client/deal-watch-app/src/App.jsx
+++ client/deal-watch-app/src/App.jsx
@@ -3,18 +3,21 @@ import HomePage from './components/HomePage/HomePage.jsx'
 import NoMatch from './components/NoMatch/NoMatch.jsx'
 import NavBar from './components/NavBar/NavBar.jsx'
 import './App.css'
+import { UserProvider } from './components/UserProvider/UserProvider.jsx'
 
 function App() {
 
   return (
     <div className='app'>
-          <BrowserRouter>
-            <NavBar></NavBar>
-            <Routes>
-              <Route path='/' element={<HomePage/>} />
-              <Route path='*' element={<NoMatch/>} />
-            </Routes>
-          </BrowserRouter>
+      <UserProvider>
+        <BrowserRouter>
+          <NavBar></NavBar>
+          <Routes>
+            <Route path='/' element={<HomePage/>} />
+            <Route path='*' element={<NoMatch/>} />
+          </Routes>
+        </BrowserRouter>
+      </UserProvider>
     </div>
   )
 }
