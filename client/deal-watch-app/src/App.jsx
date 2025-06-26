d383165c230b59632904b8a60eecfb1f4753fd7b
Jack McClure <jackmccl@meta.com>
2025-06-26 10:10:05 -0700

    Setup BrowserRouter in App.js, created NavBar component

--- client/deal-watch-app/src/App.jsx
+++ client/deal-watch-app/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomePage from './components/HomePage/HomePage.jsx'
+import NoMatch from './components/NoMatch/NoMatch.jsx'
+import NavBar from './components/NavBar/NavBar.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <div className='app'>
-
+          <BrowserRouter>
+            <NavBar></NavBar>
+            <Routes>
+              <Route path='/' element={<HomePage/>} />
+              <Route path='*' element={<NoMatch/>} />
+            </Routes>
+          </BrowserRouter>
     </div>
   )
 }
--- client/deal-watch-app/src/components/NavBar/NavBar.jsx
+++ client/deal-watch-app/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,39 @@
 import './NavBar.css'
+import { useState } from 'react'
 
 const NavBar = () => {
+    const [showUserDropdown, setShowUserDropdown] = useState(false)
+    const [testLogin, setTestLogin] = useState(true)
+
+    const toggleUserDropdown = () => {
+        setShowUserDropdown(prev => !prev)
+    }
+
     return(
         <div className='navbar'>
-            
+            <div className='navbar-pages'>
+                <a href='/'>Home</a>
+                <a href='/parts'>Parts</a>
+                <a href='/builds'>Builds</a>
+            </div>
+            <div className='navbar-user'>
+                { testLogin ? 
+                <div id='navbar-user-loggedin' className='dropdown'>
+                    <button onClick={toggleUserDropdown} class="dropdown-button">username</button>
+                    { showUserDropdown ?
+                    <div id="user-dropdown-options" class="dropdown-content">
+                        <a href="/">Link 1</a>
+                        <a href="/">Link 2</a>
+                        <a href="/">Link 3</a>
+                    </div>
+                    : null }
+                </div>
+                : 
+                <div className='navbar-user-loggedout'>
+                    <button className='navbar-user-login'>Login</button>
+                    <button className='navbar-user-signup'>Sign Up</button>
+                </div> }
+            </div>
         </div>
     )
 }
