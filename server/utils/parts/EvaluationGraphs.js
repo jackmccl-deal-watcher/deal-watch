89260165f48d96e240b617780eb10f52543716d4
Jack McClure <jackmccl@meta.com>
2025-07-03 22:47:44 -0700

    Removed unused code from unimplemented line chart

--- server/utils/parts/EvaluationGraphs.js
+++ server/utils/parts/EvaluationGraphs.js
@@ -1,5 +1,3 @@
-const { evaluatePart } = require("./EvaluateParts")
-
 const getDataPoints = (comparable_part) => {
     let time_data = []
     let price_data = []
@@ -34,49 +32,11 @@ const getAllDataPoints = (comparable_parts) => {
     return all_data_points
 }
 
-const makeListingDict = (comparable_part) => {
-    let listing_dict = {}
-    comparable_part.listing_data.map( (listing) => {
-        if (!listing_dict[listing.sold_date]) {
-            listing_dict[listing.sold_date] = []
-        }
-        listing_dict[listing.sold_date].push(listing.sold_price)
-    })
-    Object.keys(listing_dict).map( (sold_date) => {
-        let sold_price_sum = 0
-        listing_dict[sold_date].map( (price) => {
-            sold_price_sum += price
-        })
-        if (listing_dict[sold_date].length > 1) {
-            listing_dict[sold_date] = (sold_price_sum / listing_dict[sold_date].length).toFixed(2)
-        } else {
-            listing_dict[sold_date] = (sold_price_sum).toFixed(2)
-        }
-    })
-    return listing_dict
-}
-
-const makePriceDataArray = (comparable_part, X_Times) => {
-    const price_data = X_Times.map( (X_Time) => {
-        if (comparable_part.listing_dict[X_Time]) {
-            return comparable_part.listing_dict[X_Time]
-        } else {
-            return null
-        }
-    })
-    return price_data
-}
-
-const getUniqueTimes = (time_data) => {
-    const X_Times = [...new Set(time_data.map(date => date.getTime()))]
-    return [...X_Times.map(time => new Date(time))]
-}
-
-const makeDataPoints = (comparable_part) => {
-    const dataPoints = comparable_part.listing_data.map( (listing) => {
+const makePlotPoints = (comparable_part) => {
+    const plotPoints = comparable_part.listing_data.map( (listing) => {
         return {'x': listing.sold_date, 'y': listing.sold_price, 'id':comparable_part.brand + ': ' + comparable_part.model + ' - ' + listing.sold_date}
     })
-    return dataPoints
+    return plotPoints
 }
 
 const makeMovingAveragePoints = (all_data) => {
@@ -113,36 +73,12 @@ const makeMovingAveragePoints = (all_data) => {
 }
 
 const makeGraphData = (evaluation) => {
-    evaluation.comparable_parts = evaluation.comparable_parts.map( (comparable_part) => {
-        comparable_part['listing_dict'] = makeListingDict(comparable_part)
-        return comparable_part
-    })
-
     const allDataPoints = getAllDataPoints(evaluation.comparable_parts)
-
-    const X_Times = getUniqueTimes(allDataPoints.all_time_data).sort((a, b) => {
-        return b - a
-    })
-
-    const Y_Prices = []
-
-    evaluation.comparable_parts = evaluation.comparable_parts.map( (comparable_part) => {
-        comparable_part['price_data'] = makePriceDataArray(comparable_part, X_Times)
-        Y_Prices.push({
-            'data': comparable_part['price_data'],
-            'label': comparable_part.brand + ': ' + comparable_part.model,
-            'connectNulls': true,
-        })
-        return comparable_part
-    })
-
-
 
     const X_Y_Points = []
     evaluation.comparable_parts = evaluation.comparable_parts.map( (comparable_part) => {
-        comparable_part['price_data'] = makePriceDataArray(comparable_part, X_Times)
         X_Y_Points.push({
-            'data': makeDataPoints(comparable_part),
+            'data': makePlotPoints(comparable_part),
             'label': comparable_part.brand + ': ' + comparable_part.model,
         })
         return comparable_part
@@ -154,8 +90,6 @@ const makeGraphData = (evaluation) => {
         'id': '7-day-moving-average',
     }
 
-    evaluation.X_Data = X_Times
-    evaluation.Y_Data = Y_Prices
     evaluation.X_Y_Points = X_Y_Points
     evaluation.M_A_Points = M_A_Points
 
