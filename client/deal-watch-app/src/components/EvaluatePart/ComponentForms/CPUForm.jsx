1bd3d8cb10d253c6f410f677458dce0819273716
Jack McClure <jackmccl@meta.com>
2025-07-07 11:18:17 -0700

    Fixed scale for videocard and cpu sliders

--- client/deal-watch-app/src/components/EvaluatePart/ComponentForms/CPUForm.jsx
+++ client/deal-watch-app/src/components/EvaluatePart/ComponentForms/CPUForm.jsx
@@ -4,7 +4,7 @@ import { CPU_PROPERTIES } from '../../../component_enums/ComponentPropertiesEnum
 import './ComponentForm.css'
 
 const CPUForm = ({ handlePartEvaluation }) => {
-    const [cores, setCores] = useState(4)
+    const [cores, setCores] = useState(2)
     const [baseClock, setBaseClock] = useState(3200000000)
     const [boostClock, setBoostClock] = useState(3400000000)
 
@@ -29,11 +29,11 @@ const CPUForm = ({ handlePartEvaluation }) => {
     return(
         <div className='component-form'>
             <p className='component-form-input-label'>Cores: {cores}</p>
-            <Slider min={1} max={64} valueLabelDisplay='auto' valueLabelFormat={getCoresLabelText} value={cores} onChange={(e, newValue) => setCores(newValue)}></Slider>
+            <Slider min={2} max={64} step={2} valueLabelDisplay='auto' valueLabelFormat={getCoresLabelText} value={cores} onChange={(e, newValue) => setCores(newValue)}></Slider>
             <p className='component-form-input-label'>Base Clock: {getClockSpeedLabelText(baseClock)}</p>
-            <Slider min={1000000000} max={4800000000} valueLabelDisplay='auto' valueLabelFormat={getClockSpeedLabelText} value={baseClock} onChange={(e, newValue) => setBaseClock(newValue)}></Slider>
+            <Slider min={1000000000} max={4800000000} step={10000000} valueLabelDisplay='auto' valueLabelFormat={getClockSpeedLabelText} value={baseClock} onChange={(e, newValue) => setBaseClock(newValue)}></Slider>
             <p className='component-form-input-label'>Boost Clock: {getClockSpeedLabelText(boostClock)}</p>
-            <Slider min={1000000000} max={5500000000} valueLabelDisplay='auto' valueLabelFormat={getClockSpeedLabelText} value={boostClock} onChange={(e, newValue) => setBoostClock(newValue)}></Slider>
+            <Slider min={1000000000} max={5500000000} step={10000000} valueLabelDisplay='auto' valueLabelFormat={getClockSpeedLabelText} value={boostClock} onChange={(e, newValue) => setBoostClock(newValue)}></Slider>
             <button className='component-form-submit-button' onClick={cpuEvaluate}>Evaluate</button>
         </div>
     )
--- client/deal-watch-app/src/components/EvaluatePart/ComponentForms/VideoCardForm.jsx
+++ client/deal-watch-app/src/components/EvaluatePart/ComponentForms/VideoCardForm.jsx
@@ -29,11 +29,11 @@ const VideoCardForm = ({ handlePartEvaluation }) => {
     return(
         <div className='component-form'>
             <p className='component-form-input-label'>VRAM: {getVramLabelText(vram)}</p>
-            <Slider min={125000000} max={48000000000} valueLabelDisplay='auto' valueLabelFormat={getVramLabelText} value={vram} onChange={(e, newValue) => setVram(newValue)}></Slider>
+            <Slider min={1000000000} max={48000000000} step={1000000000} valueLabelDisplay='auto' valueLabelFormat={getVramLabelText} value={vram} onChange={(e, newValue) => setVram(newValue)}></Slider>
             <p className='component-form-input-label'>Base Clock: {getClockSpeedLabelText(baseClock)}</p>
-            <Slider min={115000000} max={1825000000} valueLabelDisplay='auto' valueLabelFormat={getClockSpeedLabelText} value={baseClock} onChange={(e, newValue) => setBaseClock(newValue)}></Slider>
+            <Slider min={115000000} max={1825000000} step={10000000} valueLabelDisplay='auto' valueLabelFormat={getClockSpeedLabelText} value={baseClock} onChange={(e, newValue) => setBaseClock(newValue)}></Slider>
             <p className='component-form-input-label'>Boost Clock: {getClockSpeedLabelText(boostClock)}</p>
-            <Slider min={250000000} max={2250000000} valueLabelDisplay='auto' valueLabelFormat={getClockSpeedLabelText} value={boostClock} onChange={(e, newValue) => setBoostClock(newValue)}></Slider>
+            <Slider min={250000000} max={2250000000} step={10000000} valueLabelDisplay='auto' valueLabelFormat={getClockSpeedLabelText} value={boostClock} onChange={(e, newValue) => setBoostClock(newValue)}></Slider>
             <button className='component-form-submit-button' onClick={videocardEvaluate}>Evaluate</button>
         </div>
     )
