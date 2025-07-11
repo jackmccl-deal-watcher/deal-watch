1b7968b902551818432e23e4864f2d3d06bd7cda
Jack McClure <jackmccl@meta.com>
2025-07-11 09:51:57 -0700

    Added await to Promise.all

--- server/modules/builds/GenerateBuilds.js
+++ server/modules/builds/GenerateBuilds.js
@@ -4,8 +4,8 @@ const { recommendBuild } = require("./BuildRecommender")
 const { addPartListingsToBuild } = require("./PartListing")
 
 const generateBuilds = async (userAllocations) => {
-    const [ balancedBuild, budgetBuild, performanceBuild ] = Promise.all(recommendBuild(userAllocations, MODE.BALANCED), recommendBuild(userAllocations, MODE.BUDGET), recommendBuild(userAllocations, MODE.PERFORMANCE))
-    const [ balancedBuildWithListings, budgetBuildWithListings, performanceBuildWithListings ] = Promise.all(addPartListingsToBuild(balancedBuild), addPartListingsToBuild(budgetBuild), addPartListingsToBuild(performanceBuild))
+    const [ balancedBuild, budgetBuild, performanceBuild ] = await Promise.all([recommendBuild(userAllocations, MODE.BALANCED), recommendBuild(userAllocations, MODE.BUDGET), recommendBuild(userAllocations, MODE.PERFORMANCE)])
+    const [ balancedBuildWithListings, budgetBuildWithListings, performanceBuildWithListings ] = await Promise.all([addPartListingsToBuild(balancedBuild), addPartListingsToBuild(budgetBuild), addPartListingsToBuild(performanceBuild)])
     const builds = {
         'budget_build': budgetBuildWithListings,
         'balanced_build': balancedBuildWithListings,
