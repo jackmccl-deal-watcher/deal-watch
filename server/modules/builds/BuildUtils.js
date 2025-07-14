26419f72f564ae888f03dbdab0b4cea515453f72
Jack McClure <jackmccl@meta.com>
2025-07-14 11:18:13 -0700

    Fixed build utils equation spacing for consistency

--- server/modules/builds/BuildUtils.js
+++ server/modules/builds/BuildUtils.js
@@ -3,7 +3,7 @@ const calcMixedRating = (a_rating, b_rating, allocation) => {
         return 0
     }
     return Math.sign(a_rating - b_rating) * allocation * (2/3) 
-                + allocation * (1/3) * (a_rating - b_rating) / (a_rating+b_rating)
+                + allocation * (1/3) * (a_rating - b_rating) / (a_rating + b_rating)
 }
 
 module.exports = { calcMixedRating }
