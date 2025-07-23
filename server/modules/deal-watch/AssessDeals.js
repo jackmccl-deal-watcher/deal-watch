4ba3c5c79cc4f2ff656e86c8084e4fc28b4f2b01
Jack McClure <jackmccl@meta.com>
2025-07-23 13:00:23 -0700

    Fixed component dict to have null for defined models that don't have enough listing data

--- server/modules/deal-watch/AssessDeals.js
+++ server/modules/deal-watch/AssessDeals.js
@@ -22,6 +22,7 @@ const extractComponentsFromListing = async (listing) => {
         [LISTING_PROPERTIES.DESCRIPTION]: listing[LISTING_PROPERTIES.SHORT_DESCRIPTION],
         [LISTING_PROPERTIES.PRICE]: listing[LISTING_PROPERTIES.PRICE][LISTING_PROPERTIES.VALUE],
         [LISTING_PROPERTIES.WEB_URL]: listing[LISTING_PROPERTIES.WEB_URL],
+        [LISTING_PROPERTIES.ITEM_URL]: listing[LISTING_PROPERTIES.ITEM_URL],
         [LISTING_PROPERTIES.COMPONENTS_DICT]: componentsDict,
     }
     return listingDict
@@ -57,9 +58,13 @@ const assessListing = async (listing) => {
         } else {
             definedComponentsCombinedWeight += COMPONENT_VALUE_WEIGHTS[component_type]
             const estimatedComponentValue = await estimateComponentValue(component_info)
-            listing[LISTING_PROPERTIES.COMPONENTS_DICT][component_type] = {
-                'model': component_info,
-                'estimated_value': estimatedComponentValue
+            if (estimatedComponentValue > 0) {
+                listing[LISTING_PROPERTIES.COMPONENTS_DICT][component_type] = {
+                    'model': component_info,
+                    'estimated_value': estimatedComponentValue
+                }
+            } else {
+                listing[LISTING_PROPERTIES.COMPONENTS_DICT][component_type] = null
             }
             return accumulator + estimatedComponentValue
         }
@@ -83,9 +88,7 @@ const assessDeals = async () => {
             console.error(error)
         }
     }
-    console.log(assessedPCListings)
     return assessedPCListings
 }
 
-assessDeals()
 module.exports = assessDeals
