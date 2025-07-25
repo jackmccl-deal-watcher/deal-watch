cfbf13e26f850d31dd9fa1ad42ebe2d8f2d52a1f
Jack McClure <jackmccl@meta.com>
2025-07-25 10:02:25 -0700

    Added seperate try blocks for validation, flash, and flash-lite in GeminiUtils

--- server/utils/gemini/GeminiUtils.js
+++ server/utils/gemini/GeminiUtils.js
@@ -7,6 +7,20 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY});
 const fetchGeminiResponse = async (prompt) => {
     try {
         validateStringInput(prompt, Object.keys({prompt})[0])
+    } catch (error) {
+        console.error(error)
+        return
+    }
+    try {
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: prompt,
+        });
+        return response.text
+    } catch (error) {
+        console.error(error)
+    }
+    try {
         const response = await ai.models.generateContent({
             model: "gemini-2.5-flash-lite",
             contents: prompt,
@@ -15,6 +29,7 @@ const fetchGeminiResponse = async (prompt) => {
     } catch (error) {
         console.error(error)
     }
+    console.error("Both gemini-2.5-flash & gemini-2.5-flash-lite failed.");
 }
 
 module.exports = fetchGeminiResponse
